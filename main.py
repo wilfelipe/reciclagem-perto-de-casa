374c45aa0aa8121d36d0d7696a0adea9fe8671fe
Wilson Felipe <wilsonfelipe15@gmail.com>
2019-09-28 14:18:59 -0300

    Organizando código
    Limpando código, adicionando comentários e formatando impressão para usuário.

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def haversine(coordenadasUsuario, coordenadasPonto):
   return c * R  # Retornando a distância em Kilômetros entre os dois pontos.
 
 
-# Coordenadas simulando dados digitado pelo usuário
-coordenadasUsuario = [-8.038874, -34.9330603]
+
+coordenadasUsuario = [float(input('Digite sua latitude: ')), float(input('Digite sua longitude: '))] # Entrada das coordenadas do usuário
 
 
 # Abrindo dataset com todos os pontos de coleta na região de Recife
@@ -36,34 +36,41 @@ with open('pontos-de-coletas-residuos.csv', encoding="utf8") as f:
 		Caso a tabela tenha 25 linhas, o código irá passar pelo for 25 vezes e em cada vez a variável row será uma lista com os valores daquela coluna x linha.
 		row = [tiposresiduos, bairro, endereco, complemento, observacao, latitude, longitude
 	'''
-	Menor_Distancia = 20037500
-	lista = []
+	pontosColeta = []
+	# Row[0] = Tipos de residuos
+	# Row[1] = Bairro
+	# Row[2] = Endereço
+	# Row[3] = Complemento
+	# Row[4] = Observação
+	# Row[5] = Latitude do ponto de coleta
+	# Row[6] = Longitude do ponto de coleta
 	for row in reader:
-		# Convertendo valores latitude e longitude de str para float, para que possa ser feito operações matemáticas
-		coordenadasPonto = [float(row[5]), float(row[6])]
-		lista.append(
-                    [haversine(coordenadasUsuario, coordenadasPonto), row[0], row[1], row[2], row[3], row[4], row[5], row[6]])
-		'''
-		if Menor_Distancia > haversine(coordenadasUsuario,coordenadasPonto):
-			Menor_Distancia = haversine(coordenadasUsuario,coordenadasPonto)
-			endereco = row[2]
-		'''
-		# Row[0] = Tipos de residuos
-		# Row[1] = Bairro
-		# Row[2] = Endereço
-		# Row[3] = Complemento
-		# Row[4] = Observação
-		# Row[5] = Latitude do ponto de coleta
-		# Row[6] = Longitude do ponto de coleta
+		coordenadasPonto = [float(row[5]), float(row[6])] # Convertendo valores latitude e longitude de str para float, para que possa ser feito operações matemáticas
+		
+		pontosColeta.append([haversine(coordenadasUsuario, coordenadasPonto),row[0],row[1],row[2],row[3],row[4],row[5]]) # Adicionando a distância do ponto de coleta para o usuário mais os dados desse ponto, para a matriz pontosColeta
 		'''
 			haversine(coordernada1, coordernada2) retorna a distância entre o ponto 1 e o ponto 2.
 			Ex: distancia = haversine(coordenadasUnip, coordenadasExtra)
 				print(distancia)
 				>>> 0.25645
 		'''
-lista.sort()
-print(lista[0])
-print(lista[1])
-print(lista[2])
-print(lista[3])
-print(lista[4])
+		
+pontosColeta.sort() # Organizando lista pela distância, gerada pela função haversine, do mais próximo ao mais distante.
+
+# pontosColeta[x][0] = Distância do ponto para o usuário, em Km.
+# pontosColeta[x][1] = Tipos de residuos
+# pontosColeta[x][2] = Bairro
+# pontosColeta[x][3] = Endereço
+# pontosColeta[x][4] = Complemento
+# pontosColeta[x][5] = Observação
+
+# ----- Imprindo resultado para o usuário ----
+n = 3 # Número de resultados que serão imprimidos
+print('\nPontos de coleta mais próximo de você:\n')
+for i in range(n):
+	print(pontosColeta[i][5])
+	print('Distância: ', round(pontosColeta[i][0], 2), 'Km')
+	print('Enderço: ', pontosColeta[i][3], '.', pontosColeta[i][2])
+	print('Tipos de residuos: ', pontosColeta[i][1])
+	print('---------------------------')
+input()
